566f398dc1d98048d3ba76b5dc17c2a1c0cd985c
mistercoolertyper <84140706+Mistercoolertyper@users.noreply.github.com>
2026-03-25 11:03:05 +0100

    Add default values for MonitoringData

--- LF-8-Server/MonitoredClient.cs
+++ LF-8-Server/MonitoredClient.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LF_8_Server
 {
-    internal class MonitoredClient(string hostname, string url)
-    {
-        public string Hostname = hostname;
-        private string _url = url;
-    }
+	internal class MonitoredClient(string hostname, string url)
+	{
+		private RestClient _client = new(url);
+		public MonitoringData CurrentData = new();
+		public string Hostname = hostname;
+		private string _url = url;
+	}
 }
--- LF-8-Server/MonitoringData.cs
+++ LF-8-Server/MonitoringData.cs
@@ -4,46 +4,46 @@ namespace LF_8_Server
 {
 	internal class MemoryUsage
 	{
-		public float Used { get; set; }
-		public float Total { get; set; }
-		public string Unit { get; set; }
+		public float Used { get; set; } = 0;
+		public float Total { get; set; } = 0;
+		public string Unit { get; set; } = "";
 	}
 
 	internal class DiskIO
 	{
 		[JsonProperty("readMBps")]
-		public float CurrentRead { get; set; }
+		public float CurrentRead { get; set; } = 0;
 		[JsonProperty("writeMBps")]
-		public float CurrentWrite { get; set; }
+		public float CurrentWrite { get; set; } = 0;
 	}
 
 	internal class DiskUsage
 	{
-		public string Name { get; set; }
+		public string Name { get; set; } = "";
 		[JsonProperty("usedGiB")]
-		public float TotalUsed { get; set; }
+		public float TotalUsed { get; set; } = 0;
 		[JsonProperty("totalGiB")]
-		public float Total { get; set; }
+		public float Total { get; set; } = 0;
 	}
 
 	internal class NetworkInterface
 	{
 		[JsonProperty("interface")]
-		public string Name { get; set; }
+		public string Name { get; set; } = "";
 		[JsonProperty("downloadMbit")]
-		public float CurrentDownload { get; set; }
+		public float CurrentDownload { get; set; } = 0;
 		[JsonProperty("uploadMbit")]
-		public float CurrentUpload { get; set; }
+		public float CurrentUpload { get; set; } = 0;
 	}
 
 	internal class MonitoringData
 	{
-		public string Timestamp { get; set; }
+		public string Timestamp { get; set; } = "";
 		[JsonProperty("ram")]
-		public MemoryUsage Memory { get; set; }
-		public DiskIO DiskIO { get; set; }
-		public DiskUsage[] Disks { get; set; }
+		public MemoryUsage Memory { get; set; } = new();
+		public DiskIO DiskIO { get; set; } = new();
+		public DiskUsage[] Disks { get; set; } = [];
 		[JsonProperty("network")]
-		public NetworkInterface[] NetworkInterfaces { get; set; }
+		public NetworkInterface[] NetworkInterfaces { get; set; } = [];
 	}
 }
